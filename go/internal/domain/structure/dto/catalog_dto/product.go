7f86523d9ebfce46d9c060be2c4116b048f658e0
danielfleancu <fleancu.daniel@gmail.com>
2025-01-21 12:28:34 +0200

    ...

---
+++ go/internal/domain/structure/dto/catalog_dto/product.go
@@ -0,0 +1,44 @@
+package catalog_dto
+
+import (
+	"database/sql"
+)
+
+type GenerateNamesInputDto struct {
+	IdGroup  int   `json:"id_group"`
+	IdsChars []int `json:"ids_chars"`
+	IdsVal   []int `json:"ids_val"`
+}
+
+type ProductCharQueryDto struct {
+	IdProduct int    `db:"id_product"`
+	Name      string `db:"name"`
+	UseInName bool   `db:"use_product_name"`
+	UseEmoji  bool   `db:"add_emodji"`
+	Position  string `db:"position"`
+}
+
+type ProductNameStoreDto struct {
+	Id        int    `db:"id"`
+	Name      string `db:"name"`
+	UpdatedAt string `db:"updated_at"`
+}
+
+type ProductSortQueryDto struct {
+	ID             int            `db:"id"`
+	IdSubcategory  int            `db:"id_subcategory"`
+	IdCategory     int            `db:"id_category"`
+	IdBrand        int            `db:"id_brand"`
+	IdGroup        int            `db:"id_group"`
+	BrandPosition  string         `db:"brand_position"`
+	CatPosition    string         `db:"cat_position"`
+	SubCatPosition string         `db:"subcat_position"`
+	PageUp         int            `db:"page_up"`
+	Position       sql.NullString `db:"position"`
+}
+
+type ProductSortStoreDto struct {
+	ID        int    `db:"id"`
+	Position  int    `db:"page_up"`
+	UpdatedAt string `db:"updated_at"`
+}
